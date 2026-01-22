128f7fa5fbc8bad3b827f6bf6c50e4eff5f59884
Md Abdul Ahad <abd.me7@gmail.com>
2026-01-22 10:47:32 +0600

    API calling

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,11 +1,21 @@
+'use-client'
 import React from 'react';
 
-const foods = () => {
+const getFoods = async () => {
+    const res = await fetch('https://taxi-kitchen-api.vercel.app/api/v1/foods/random');
+    const data = await res.json();
+    console.log(data);
+    return data.foods || [];
+}
+const Foods = async () => {
+    const foods = await getFoods();
     return (
         <div>
-            Foods
+            <h2 className='text-4xl font-bold'>
+                Total <span className='text-yellow-500'>{foods.length}</span> Foods Found
+            </h2>
         </div>
     );
 };
 
-export default foods;
+export default Foods;
